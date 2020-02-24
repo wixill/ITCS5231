f1259c39163f13be006bce77b4b074034c414846
Jane <jehiltz01@icloud.com>
2020-02-24 12:32:17 -0500

    created dev branch, added castle and rune assets, started implementing activators and puzzle objects

---
+++ Runic/Assets/Scripts/Activators/Activator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Parent class Activator
+// Has a list of PuzzleObjects that it then activates
+public class Activator : MonoBehaviour
+{
+    // List of puzzle objects to activate
+    [SerializeField] private List<PuzzleObject> toActivate;
+
+    // Activates each puzzle object in the toActivate list
+    public void activate()
+    {
+        foreach(PuzzleObject x in toActivate)
+        {
+            x.activate();
+        }
+    }
+
+}
---
+++ Runic/Assets/Scripts/Activators/Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Child of Activator
+// Activates once player is nearby and presses E to interact
+// Then the button will turn on/off and activates the coresponding PuzzleObject
+public class Button : Activator
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
