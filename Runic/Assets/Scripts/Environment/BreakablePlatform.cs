b4c853ad78a6fbfe962084a908c9f933b8a26b73
Jane <jehiltz01@icloud.com>
2020-03-10 17:44:16 -0400

    Added breakable platform prefab and improved fractured block prefab by making it fade and then destroy itself after a timer

---
+++ Runic/Assets/Scripts/Environment/BreakablePlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A platform/block that breaks after a certain amount of time once something is placed on top of it or the player walks over it
+public class BreakablePlatform : MonoBehaviour
+{
+    // Broken block to replace platform
+    [SerializeField] private GameObject fracturedBlock;
+    // Scale factor to ensure it scales to the correct size as the original object - defaulted to 0.5 to match a default cube
+    [SerializeField] private float scaleFactor = 0.5f;
+    
+    // How long until it breaks after being stepped on
+    [SerializeField] private float breakCountdown;
+    private bool countdownStart = false;
+
+    // When player steps on a platform (enters the trigger box) then start the timer
+    private void OnTriggerEnter(Collider other)
+    {
+        countdownStart = true;
+        Debug.Log("scale vector " + new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdownStart)
+        {
+            breakCountdown -= Time.deltaTime;
+            if(breakCountdown <= 0)
+            {
+                GameObject broken = Instantiate(fracturedBlock, transform.position, transform.rotation);
+                broken.transform.localScale = new Vector3(this.transform.localScale.x * scaleFactor, this.transform.localScale.y * scaleFactor, this.transform.localScale.z * scaleFactor);
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Runic/Assets/Scripts/Environment/DestroyTimer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Destroys object after a certain amount of time
+// Object qickly fades out before destroying itself
+// Object's shader must be set to Fade or Transparent in order for the fade to work
+public class DestroyTimer : MonoBehaviour
+{
+    // How long until it destroys itself after creation
+    [SerializeField] private float destroyCountdown;
+    private bool countdownStart = false;
+
+    // Used to change the object's transparency
+    private Color alphaColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownStart = true;
+        alphaColor = GetComponent<MeshRenderer>().material.color;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy after a certain amount of time
+        if (countdownStart)
+        {
+            destroyCountdown -= Time.deltaTime;
+            if (destroyCountdown <= 0)
+            {
+                // When its time to destroy, quickly fade out then destroy object
+                Color newColor = alphaColor;
+                newColor.a -= Time.deltaTime;
+                if (newColor.a >= 0)
+                {
+                    alphaColor = newColor;
+                    GetComponent<MeshRenderer>().material.color = alphaColor;
+                } else
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
