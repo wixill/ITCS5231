96bdfc08f1cc044b753187e93e85368a6a93496a
Jane <jehiltz01@icloud.com>
2020-03-17 13:20:09 -0400

    Mostly finished outside scene

---
+++ Runic/Assets/Scripts/Environment/DeathBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBox : MonoBehaviour
+{
+    // If player collides with Death Box, restart from checkpoint
+    // (Right now just restart the game)
+    // TODO: Update to restart to checkpoint
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DEATH TRIGGERED");
+        if (other.gameObject.CompareTag("Player")){
+            Debug.Log("PLAYER DIES");
+            string currentSceen = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceen);
+        }
+    }
+}
