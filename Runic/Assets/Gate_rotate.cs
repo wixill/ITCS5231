2f36327e1b4da55ec11dd383558c1f4e530a37b3
Jane <jehiltz01@icloud.com>
2020-03-30 21:51:15 -0400

    Implemented pressure plate activator that raises an animated gate

---
+++ Runic/Assets/Gate_rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate_rotate : MonoBehaviour
+{
+    private Vector3 pivotPoint;
+
+    private void Start()
+    {
+        pivotPoint = transform.position;
+        pivotPoint.y = 0;
+        pivotPoint.z = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(pivotPoint, Vector3.up, 30 * Time.deltaTime);
+    }
+}
