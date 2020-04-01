03ed23dfbc7b08d096c55d6780977c65a3b8711a
Jane <jehiltz01@icloud.com>
2020-03-31 20:16:15 -0400

    Created Scene Transition Prefab
    - Added fade to black animation
    - Added SceneTransition.cs: when triggered, goes to given scene
    - Added TriggerParent.cs: when triggered, calls parent's trigger
    - Combined into prefab that transitions scenes when player collides w/
    it
    - Created insideCastle scene

---
+++ Runic/Assets/Scripts/TriggerParent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerParent : MonoBehaviour
+{
+    [SerializeField] private GameObject parent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        parent.SendMessage("OnTriggerEnter", other);
+    }
+}
---
+++ Runic/Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator anim;
+    [SerializeField] int nextSceneIndex;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // When player enters trigger, switch to the desired next scene
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("FadeOut");
+        }
+    }
+
+    public void onFadeComplete()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
