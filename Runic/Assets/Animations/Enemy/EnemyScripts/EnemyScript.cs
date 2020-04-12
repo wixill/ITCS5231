0231ff82fcb12bc6e1beaf908ca0693d0cf31e6d
Marcus Lofton <mlofton4@uncc.edu>
2020-04-12 00:11:22 -0400

    Fixed a bug with the enemy walking animation (will do more testing but I think its finally working)

--- Runic/Assets/Animations/Enemy/EnemyScripts/EnemyScript.cs
+++ Runic/Assets/Animations/Enemy/EnemyScripts/EnemyScript.cs
@@ -8,13 +8,17 @@ public class EnemyScript : MonoBehaviour
     static Animator anim;
     [SerializeField] private Transform trans;
     [SerializeField] private Transform Playertrans;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float distanceOffset;
+    [SerializeField] private float angleOffset;
+
 
     private float maxSpeed;
     private float radiusOfSat;
     private float turnSpeed;
     //Vector that will hold the target position
     Vector3 targetPosition;
-
+    Vector3 targetPoint;
     //Vector that will hold the direction of the target
     Vector3 seeTarget;
 
@@ -29,32 +33,40 @@ public class EnemyScript : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
         radiusOfSat = 1f;
         turnSpeed = 3f;
-        maxSpeed = 3f;
+        maxSpeed = 1.5f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if the distance between the enemy and the player is greater than 5, we will move the enemy towards the player for now
-        if (Vector3.Distance(trans.position, Playertrans.position) > 10f)
+        //goes to the target location
+        GoToTargetPos();
+
+        if (trans.position == targetPosition)
         {
-            //goes to the target location
-            GoToTargetPos();
-        }
-        
-        //will move the character if moving is true
-        if (moving)
-        {
-            MovePlayer();
-            
+            rb.velocity = Vector3.zero;
+            moving = false;
+            anim.SetBool("isMoving", moving);
         }
 
         else
         {
-            Shoot();
+
+            //will move the character if moving is true
+            if (moving)
+            {
+                MovePlayer();
+
+            }
+
+            else
+            {
+                Shoot();
+            }
         }
     }
 
@@ -62,8 +74,17 @@ public class EnemyScript : MonoBehaviour
     void GoToTargetPos()
     {
         //sets the direction of the position
-        seeTarget = new Vector3(Playertrans.position.x - trans.position.x, 1f, Playertrans.position.z - trans.position.z);
-        
+        seeTarget = new Vector3(Playertrans.position.x - trans.position.x, trans.position.y, Playertrans.position.z - trans.position.z);
+
+        //Gets the point forward from the leader's foward facing vector
+        targetPoint = Playertrans.forward * distanceOffset;
+
+        //sets the position in the formation
+        targetPosition = Quaternion.Euler(0f, angleOffset, 0f) * targetPoint;
+
+        //updates enemy position in the formation
+        targetPosition += Playertrans.position;
+        targetPosition = new Vector3(targetPosition.x, 0f, targetPosition.z);
         //allows the character to rotate to the target position
         pRotate = Quaternion.LookRotation(seeTarget);
         moving = true;     
@@ -74,23 +95,13 @@ public class EnemyScript : MonoBehaviour
         //rotates the enemy to the player's position
         trans.rotation = Quaternion.Lerp(trans.rotation, pRotate, turnSpeed * Time.deltaTime);
 
-        //moves the enemy to the target position
-        trans.position = Vector3.MoveTowards(Playertrans.position, targetPosition, maxSpeed * Time.deltaTime);
+        trans.position = Vector3.MoveTowards(trans.position, targetPosition, maxSpeed * Time.deltaTime);
 
         //sets the speed in Animator 
-        anim.SetFloat("maxSpeed", maxSpeed * Time.deltaTime);
+        //anim.SetFloat("Speed", maxSpeed * Time.deltaTime);
 
         //sets the boolean in Animator. since moving is true, this will make the enemy move
         anim.SetBool("isMoving", moving);
-
-        //if we have reached the target destination
-        if (Vector3.Distance(trans.position, Playertrans.position) < 10f)
-        {
-            //gets our character to stop moving and then sets the boolean in Animator to false
-            moving = false;
-            anim.SetBool("isWalking", moving);
-        }
-
     }
 
     void Shoot()
